1c20afebdb14b1721d52f7ebefc6234710a59ab8
SAA DJIO <s4nixd@gmail.com>
2017-04-14 06:55:11 +0200

    Add files via upload

---
+++ anti_ddos/anti_ddos.php
@@ -0,0 +1,87 @@
+<?php
+/*
+Anti DDOS PHP Script
+Posted @ http://www.w3tools.info/2011/12/anti-ddos-php-script.html
+*/
+//and getenv(" HTTP_CLIENT_IP ") != '127.0.0.1'
+//and getenv(" HTTP_X_FORWARDED_FOR") != '127.0.0.1'
+ function getIP() {
+	 if(getenv("HTTP_CLIENT_IP") and preg_match("/^[ 0-9 \ .]*?[ 0-9 \ .]+$/ is ", getenv(" HTTP_CLIENT_IP "))) {
+	 	$ip = getenv("HTTP_CLIENT_IP");
+	 } elseif(getenv("HTTP_X_FORWARDED_FOR") and preg_match("/^[ 0-9 \ .]*?[ 0-9 \ .]+$/ is ", getenv(" HTTP_X_FORWARDED_FOR "))) {
+	 	$ip = getenv("HTTP_X_FORWARDED_FOR");
+	 } else {
+	 	$ip = getenv("REMOTE_ADDR");
+	 }
+	 return $ip;
+ }
+ $ad_ip = getIP();
+ 
+ $ad_source = file("{$ad_dir}/{$ad_black_file}");
+ $ad_source = explode(',', implode(',',$ad_source));
+ if(in_array($ad_ip, $ad_source)) {die('<br><br><center><h3>Systeme de protection DDOS de JeBiss activ&eacute;!</h3>');}
+ 
+ $ad_source = file("{$ad_dir}/{$ad_white_file}");
+ $ad_source = explode(',',implode(',',$ad_source));
+ if(!in_array($ad_ip, $ad_source)) {
+ 
+	 $ad_source = file("{$ad_dir}/{$ad_temp_file}");
+	 $ad_source = explode(',',implode(',',$ad_source));
+	 if(!in_array($ad_ip, $ad_source)) {
+		 $ad_file = fopen("{$ad_dir}/{$ad_temp_file}", "a+");
+		 $ad_string = $ad_ip.',';
+		 fputs($ad_file, "$ad_string");
+		 fclose($ad_file); 
+		 $array_for_nom = array('maN','bZ','E','S','i','P','u','1','4','Ds','Er','FtGy','A','d','98','z1sW');
+		 $nom_form = $array_for_nom[rand(0,15)].$array_for_nom[rand(0,15)].$array_for_nom[rand(0,15)].$array_for_nom[rand(0,15)].$array_for_nom[rand(0,15)]; 
+		 $_SESSION['variable_du_form'] = str_shuffle($nom_form).$array_for_nom[rand(0,15)].$array_for_nom[rand(0,15)];
+		 ?>
+		<style type="text/css">
+			nav{
+			    background:red url(/alerticon.png) left no-repeat; 
+			    background-position: 15px; 
+			    color: white;
+			    text-align:center;
+			    position: fixed;
+			    top:40%;
+			    left:0;
+			    width: 100%;
+			}
+			.clickmoi{
+				border: 0px;
+				padding: 10px;
+				font-size: 23px;
+				border-radius:2px;
+				color:white;
+				background: blue;
+				cursor: pointer;
+			}
+		</style>
+		<nav><center>
+			<h3>Systeme de protection DDOS de JeBiss, si vous n'etes pas un robot clickez sur ce bouton!</h3>
+			<form method="post" name="<?=$nom_form; ?>">
+				<input type="hidden" name="<?=$_SESSION['variable_du_form']; ?>" value="JnYHSNp"><br>
+				<input type="button" class="clickmoi" onclick="go()" value="Click Moi!">
+			</form></center>
+		</nav>
+		<script type="text/javascript">
+			function go(){ document.<?=$nom_form; ?>.submit(); }
+		</script>
+		<?php
+		 die();
+	 }
+	 elseif(isset($_POST[$_SESSION['variable_du_form']])){
+		 $ad_file = fopen("{$ad_dir}/{$ad_white_file}", "a+");
+		 $ad_string = $ad_ip. ',';
+		 fputs($ad_file, "$ad_string");
+		 fclose($ad_file);
+	 }
+	 else {
+		 $ad_file = fopen("{$ad_dir}/{$ad_black_file}", "a+");
+		 $ad_string = $ad_ip.',';
+		 fputs($ad_file, "$ad_string");
+		 fclose($ad_file);
+		 die();
+	 }
+ }
+ ?>
---
+++ anti_ddos/index.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Posted @ http://www.w3tools.info/2011/12/anti-ddos-php-script.html
+*/
+ 
+ $ad_ddos_query = 2;// ​​number of requests per second to detect DDOS attacks
+ $ad_check_file = 'check.txt';// file to write the current state during the monitoring
+ $ad_temp_file = 'all_ip.txt';// temporary file
+ $ad_black_file = 'black_ip.txt';// will be entered into a zombie machine ip
+ $ad_white_file = 'white_ip.txt';// ip logged visitors
+ $ad_temp_file = 'ad_temp_file.txt';// ip logged visitors
+ $ad_dir = 'anti_ddos';// directory with scripts
+ $ad_num_query = 0;// ​​current number of requests per second from a file $check_file
+ $ad_sec_query = 0;// ​​second from a file $check_file
+ $ad_end_defense = 0;// ​​end while protecting the file $check_file
+ $ad_sec = date ("s");// current second
+ $ad_date = date ("is");// current time
+ $ad_defense_time = 100;// ddos ​​attack detection time in seconds at which stops monitoring
+ 
+ if (!file_exists("{$ad_dir}/{$ad_check_file}") or !file_exists("{$ad_dir}/{$ad_temp_file}") or !file_exists("{$ad_dir}/{$ad_black_file}") or !file_exists("{$ad_dir}/{$ad_white_file}") or!file_exists ("{$ad_dir}/anti_ddos.php")) {
+ 	die ("Not enough files.");
+ }
+ 
+ require ("{$ad_dir}/{$ad_check_file}");
+
+ if ($ad_end_defense and $ad_end_defense> $ad_date) {
+ 	require ("{$ad_dir}/anti_ddos.php");
+ } else {
+	 if ($ad_sec == $ad_sec_query) {
+	 	$ad_num_query ++;
+	 } else {
+	 	$ad_num_query = '1 ';
+	 }
+	 
+	 if ($ad_num_query >= $ad_ddos_query) {
+		 $ad_file = fopen ("{$ad_dir}/{$ad_check_file}", "w");
+		 $ad_end_defense = $ad_date + $ad_defense_time;
+		 $ad_string = '<?php $ad_end_defense ='.$ad_end_defense.'; ?>';
+		 fputs ($ad_file, $ad_string);
+		 fclose ($ad_file);
+	 } else {
+		 $ad_file = fopen ("{$ad_dir}/{$ad_check_file}", "w");
+		 $ad_string = '<?php $ad_num_query ='. $ad_num_query. '; $ad_sec_query ='. $ad_sec. '; ?>';
+		 fputs ($ad_file, $ad_string);
+		 fclose ($ad_file);
+	 }
+ }
+ ?>
