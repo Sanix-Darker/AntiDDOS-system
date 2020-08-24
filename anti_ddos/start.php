a978b320dffed8a9bb5e8c3e264a8e375f24d4dd
sanix-darker <s4nixd@gmail.com>
2020-08-24 12:17:15 +0100

    Fix Parse error: syntax error, unexpected if =0

--- anti_ddos/start.php
+++ anti_ddos/start.php
@@ -65,10 +65,10 @@ if(isset($_SESSION['standby'])){
 	if ($ad_end_defense and $ad_end_defense> $ad_date) {
 		require ("{$ad_dir}/../anti_ddos.php");
 	} else {
-		$ad_num_query = ($ad_sec == $ad_sec_query) ? $ad_num_query++ : '1 ';
+		$ad_num_query = ($ad_sec == $ad_sec_query) ? $ad_num_query++ : '1';
 		$ad_file = fopen ("{$ad_dir}/{$ad_check_file}", "w");
 
-		$ad_string = ($ad_num_query >= $ad_ddos_query) ? '<?php $ad_end_defense='.ltrim(($ad_date + $ad_defense_time), "0").'; ?>' : '<?php $ad_num_query='.ltrim($ad_num_query, "0").'; $ad_sec_query='.ltrim($ad_sec, "0").'; ?>';
+		$ad_string = ($ad_num_query >= $ad_ddos_query) ? '<?php $ad_end_defense='.ltrim(($ad_date + $ad_defense_time), "0").'; ?>' : '<?php $ad_num_query='.(($ad_num_query == "0" || $ad_num_query == "00") ? "0" : ltrim($ad_num_query, "0")).'; $ad_sec_query='.(($ad_sec == "0" || $ad_sec == "00") ? "0" : ltrim($ad_sec, "0")).'; ?>';
 
 		fputs ($ad_file, $ad_string);
 		fclose ($ad_file);
